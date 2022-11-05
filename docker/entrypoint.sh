4be10ef71fcc8a420a9a05e5e74dcc73d67c6887
Mateus Menezes <mateus.meneses@fieb.org.br>
2022-11-05 01:24:01 -0300

    Move ORB-SLAM3 installation to the entrypoint

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -12,4 +12,26 @@ if [ ! -f "${HOME}/.bashrc" ]; then
     echo "source /opt/ros/${ROS_DISTRO}/setup.bash" >> ~/.bashrc
 fi
 
+if [ ! -d "${HOME}/ORB_SLAM3" ]; then
+    git clone https://github.com/mateusmenezes95/ORB_SLAM3.git ORB_SLAM3 &&\
+    cd ORB_SLAM3 && \
+    chmod +x build.sh && \
+    bash ./build.sh
+
+    # TODO: Enable ROS installation of ORBSLAM3
+    # RUN apt-get update && \
+    #   apt-get install -q -y --no-install-recommends \
+    #   ros-${ROS_DISTRO}-tf \
+    #   ros-${ROS_DISTRO}-cv-bridge && \
+    #   apt-get -y autoremove && \
+    #   apt-get clean autoclean && \
+    #   rm -fr /var/lib/apt/lists/{apt,dpkg,cache,log} /tmp/* /var/tmp/*
+
+    # RUN cd /opt/ORB_SLAM3 && \
+    #   sh /opt/ros/${ROS_DISTRO}/setup.sh && \
+    #   ./build_ros.sh
+fi
+
+export LD_LIBRARY_PATH="${HOME}/ORB_SLAM3/lib"
+
 exec "$@"
